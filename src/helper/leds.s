e0ff875145d013f41d62a4779b57877246c1dcd0
Tilda Payne <tilda3payne@email.com>
2026-07-28 11:30:47 +1000

    Added files

---
+++ src/helper/leds.s
@@ -0,0 +1,23 @@
+#////////////////////////////////////////////////////////////////////////////////
+# COURSE: DESN2000
+# GROUP: T11D_A
+# FUNCTION: leds.c
+# PURPOSE: This file contains the implementation of the LED control functionality.
+# DEPENDENTS:
+#    - leds.c
+#           |
+#        - blinds.c
+#                |
+#            - evening_routine.c
+#            - morning_routine.c
+#                         |
+#                       display.c
+#leds:
+# - heating and cooling (red = heating, blue = cooling)
+# - blinds: red(closed), blue(open), yellow(half)
+#- coffee machine: red(off), blue(on)
+# - do not disturb: red(off), green(on)
+# - doorbell: red(off), white(on)
+#
+#////////////////////////////////////////////////////////////////////////////////
+
